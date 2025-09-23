4df27ee6dec29bdfb3cc4632646e0f16de489a63
corentin-jeusset <corentin.jeusset@edu.esiee.fr>
2025-09-23 06:40:11 +0000

    programme sur les palindromes

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
-#### Fonction secondaire
+#### Fonctions secondaires
 
+modif = {
+    "à": "a",
+    "ä": "a",
+    "â": "a",
+    "ç": "c",
+    "é": "e",
+    "è": "e",
+    "ê": "e",
+    "ë": "e",
+    "ï": "i",
+    "î": "i",
+    "ô": "o",
+    "ù": "u",
+    "ü": "u",
+    "û": "u",
+    " ": "",
+    "-": "",
+    ",": "",
+    "'": "",
+    "?": "",
+    "!": "",
+    ".": "",
+    ":": ""
+}
+
+def orthographe(n):
+    "Permet de désaccentuer les mots et de retirer la ponctuation"
+    n = n.lower()
+    for cle, valeur in modif.items():
+        n = n.replace(cle, valeur)
+    return n
 
 def ispalindrome(p):
-
-    # votre code ici
-    
-    return False
+    "Permet de déterminer si un mot est un palindrome ou non"
+    p = orthographe(p)
+    for i in range (len(p) // 2):
+        if p[i] != p[-i - 1]:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
-
-    # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
